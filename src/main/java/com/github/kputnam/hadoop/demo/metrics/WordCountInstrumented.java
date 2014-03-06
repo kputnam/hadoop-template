b23335fa99d102a3c2d375fd3f1bb1882a13ad7d
Kyle Putnam <kcputnam@dstsystems.com>
2014-03-05 18:34:04 -0600

    Simulate metrics for longer-running job by artificially slowing down

--- src/main/java/com/github/kputnam/hadoop/demo/metrics/WordCountInstrumented.java
+++ src/main/java/com/github/kputnam/hadoop/demo/metrics/WordCountInstrumented.java
@@ -12,6 +12,7 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 
 import java.io.IOException;
+import java.util.Random;
 import java.util.StringTokenizer;
 
 public class WordCountInstrumented extends Configured implements Tool {
@@ -63,6 +64,7 @@ public class WordCountInstrumented extends Configured implements Tool {
     public static class mapper extends InstrumentedMapper<LongWritable, Text, Text, IntWritable> {
         com.codahale.metrics.Meter     mMeter = Metrics.meter("wc.meter");
         com.codahale.metrics.Histogram mHisto = Metrics.histogram("wc.histo");
+        Random prng = new Random();
 
         private Text word = new Text("");
         private IntWritable one = new IntWritable(1);
@@ -78,6 +80,9 @@ public class WordCountInstrumented extends Configured implements Tool {
                 mHisto.update(word.getLength());
                 mMeter.mark();
                 ctx.write(word, one);
+
+                if (mHisto.getCount() % 50 == 0)
+                    Thread.sleep(Math.abs((long) (100 * prng.nextGaussian())) + 1);
             }
         }
     }
@@ -86,6 +91,7 @@ public class WordCountInstrumented extends Configured implements Tool {
     public static class combiner extends InstrumentedReducer<Text, IntWritable, Text, IntWritable> {
         com.codahale.metrics.Timer   mTimer = Metrics.timer("wc.timer");
         com.codahale.metrics.Counter mCount = Metrics.counter("wc.count");
+        Random prng = new Random();
 
         @Override
         protected void reduce(Text word, Iterable<IntWritable> counts, Context ctx)
@@ -100,6 +106,9 @@ public class WordCountInstrumented extends Configured implements Tool {
             timer.stop();
 
             ctx.write(word, new IntWritable(sum));
+
+            if (mCount.getCount() % 50 == 0)
+                Thread.sleep(Math.abs((long) (100 * prng.nextGaussian())) + 1);
         }
     }
 
