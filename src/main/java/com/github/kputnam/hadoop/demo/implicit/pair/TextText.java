f00b89dfe9dabcb21d19faf7ccaccfa0b86deb94
kputnam <putnam.kyle@gmail.com>
2014-04-09 06:01:30 -0500

    More cleanup

--- src/main/java/com/github/kputnam/hadoop/demo/implicit/pair/TextText.java
+++ src/main/java/com/github/kputnam/hadoop/demo/implicit/pair/TextText.java
@@ -7,10 +7,6 @@ import org.apache.hadoop.io.Text;
  * Created by kputnam on 4/9/14.
  */
 public class TextText extends Pair<Text, Text> {
-    public static TextText of(Text fst, Text snd) {
-        return new TextText(fst, snd);
-    }
-
     public TextText() {
         super();
     }
